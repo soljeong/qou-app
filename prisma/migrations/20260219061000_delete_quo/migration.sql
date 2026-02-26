da4fc5db39c8ab5c8d14a08b570683adf3735fef
jeongsol <jeongsol@outlook.com>
2026-02-26 12:50:56 +0900

    db push로 변경

--- prisma/migrations/20260219061000_delete_quo/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "QuoteItem" DROP CONSTRAINT "QuoteItem_quoteId_fkey";
-
--- AddForeignKey
-ALTER TABLE "QuoteItem" ADD CONSTRAINT "QuoteItem_quoteId_fkey" FOREIGN KEY ("quoteId") REFERENCES "Quote"("id") ON DELETE CASCADE ON UPDATE CASCADE;
